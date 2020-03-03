ec5579eadf5f998a76839b3f7bf7eb579d9d3c6d
Stepanenko Oksana <lactivo4ka@gmail.com>
2020-03-03 23:47:20 +0200

    feat: add transform-array task solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,45 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+   
+    if(!Array.isArray(arr)) {
+        throw new TypeError("Not array");
+    }
+
+    let transformed = [];
+
+    for(let i = 0; i < arr.length; ) {
+
+        switch(arr[i]) {
+
+            case '--discard-next':
+                i += 2; 
+                break;
+
+            case '--discard-prev':
+                if(i != 0) {
+                    transformed.pop(); 
+                }
+                i++;
+                break;
+
+            case '--double-next':
+                if(i != arr.length - 1) {
+                    transformed.push(arr[i + 1]);
+                } 
+                i++;
+                break;
+                
+            case '--double-prev':
+                if(i != 0) {
+                    transformed.push(arr[i - 1]);  
+                }
+                i++;
+                break;
+
+            default: 
+                transformed.push(arr[i]);
+                i++;    
+        }   
+    } 
+
+    return transformed;
 };
