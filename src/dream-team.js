60de094e452e9562f7efbd9720465b02e5afaff4
Stepanenko Oksana <lactivo4ka@gmail.com>
2020-03-04 00:12:52 +0200

    feat: add dream-team task solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,18 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+
+  if(Array.isArray(members)) {
+
+    let name = [];
+
+    members.map(el => {
+      if(typeof (el) === "string") {
+        let myel = el.trimLeft();
+        name.push(myel[0].toUpperCase());
+      }
+    })
+
+    return name.sort().join('');
+  }
+
+  return false;
 };
