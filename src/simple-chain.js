c8551f9080aeb9852170a906992963aea4ddfd7a
Stepanenko Oksana <lactivo4ka@gmail.com>
2020-03-09 20:11:26 +0200

    feat: add simple-chain task solution

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,52 @@
+
+
 const chainMaker = {
+
+  chain: [],
+
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+
+    return this.chain.length;
   },
+
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+
+    if(value === undefined) {
+
+      this.chain.push( '( )' );
+
+    } else {
+
+      this.chain.push(`${value}`);
+
+    }
+
+    return this;
   },
+
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+
+    if(typeof (position) === "number" && position > 0 && position <= this.chain.length) {
+
+      this.chain.splice(position - 1, 1);
+      return this;
+    }
+
+    this.chain = [];
+    throw new Error();
   },
+
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+
+    this.chain.reverse();
+    return this;
   },
+
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    
+    let myChain = this.chain.join(" )~~( ");
+    this.chain = [];
+    return '( ' + myChain + ' )';
   }
 };
 
